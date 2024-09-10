d7b8ed269bbe23f6948ddd8120da88bb7bd7f712
JJWithNoSauce <93840770+JJWithNoSauce@users.noreply.github.com>
2024-09-10 21:13:24 +0700

    First Commit!

---
+++ src/test/java/com/principle/checkinproject/CheckinprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.principle.checkinproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
